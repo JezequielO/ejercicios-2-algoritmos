3dead4248ca566e38ab28372571a37c485d8c9aa
Ezequiel Alas <cheke_5@hotmail.com>
2016-08-06 13:07:16 -0500

    ejercicio 5 semana 2 algoritmo

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float v1;
+            Console.Clear();
+            Console.WriteLine("Area de un Circulo y su perimetro");
+            Console.WriteLine("Digite radio del circulo");
+            v1 = float.Parse(Console.ReadLine());
+            double v3 = 2 * 3.1416 * v1;//perimetro
+            double v4 = 3.1416 * (Math.Pow(v1, 2));//area
+            Console.WriteLine("Area del Ciruclo: " + v4 + "cm^2");
+            Console.WriteLine("Perimetro del Ciruclo: " + v3 + "cm");
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
+
